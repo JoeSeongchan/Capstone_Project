2c9b194e531a30c23cdf16090ddf9840efdb294b
juyeop_lee <guylee97@naver.com>
2021-11-28 19:55:59 +0900

    profile

--- app/src/main/java/com/example/capstone/upload_option.java
+++ app/src/main/java/com/example/capstone/upload_option.java
@@ -6,11 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class upload_option extends AppCompatActivity {
     private TextView direct,record;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private String userid="IOxZt7qFPxdE93PNtElzSztmYpP2";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,16 +54,27 @@ public class upload_option extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 
     @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 1 && resultCode == RESULT_OK) {
             Uri fileUri = data.getData();
+            StorageReference s_ref = storage.getReference();
+            StorageReference r_ref = s_ref.child(userid+"/"+fileUri.getLastPathSegment());
+            UploadTask uploadTask = r_ref.putFile((fileUri));
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(upload_option.this, "응안돼~", Toast.LENGTH_SHORT).show();
+                }
+            });
 
         }
+
+
+
     }
 
 
